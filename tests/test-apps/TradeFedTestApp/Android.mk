96aa4eb6d94848099adb5afdaeb8c320a9698163
Brett Chabot <brettchabot@android.com>
2010-04-28 17:28:18 -0700

    Tweak TradeFedTestApp to fix install on older platforms after recent build changes.
    SDK API level '4' is used because thats the lowest API level supported by
    build system.
    
    Change-Id: I87ccc8a7d0d19fa3a3a4dc678460f6cd6d7fecad

--- tests/test-apps/TradeFedTestApp/Android.mk
+++ tests/test-apps/TradeFedTestApp/Android.mk
@@ -20,7 +20,7 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_SDK_VERSION := current
+LOCAL_SDK_VERSION := 4
 
 LOCAL_PACKAGE_NAME := TradeFedTestApp
 
