559538d256b4bf5699042dc36a3a607a646ffadf
Brett Chabot <brettchabot@android.com>
2011-04-18 11:52:46 -0700

    Fix device manager unit tests.
    Change-Id: If879ccde004b97c81d193b7b4a785531eeead177

--- tests/src/com/android/tradefed/device/DeviceManagerTest.java
+++ tests/src/com/android/tradefed/device/DeviceManagerTest.java
@@ -240,6 +240,7 @@ public class DeviceManagerTest extends TestCase {
         DeviceSelectionOptions options = new DeviceSelectionOptions();
         options.setEmulatorRequested(true);
         EasyMock.expect(mMockAdbBridge.getDevices()).andReturn(new IDevice[] {});
+        EasyMock.expect(mMockDeviceFactory.createDevice()).andReturn(mMockTestDevice);
         replayMocks();
         DeviceManager manager = createDeviceManager();
         assertNotNull(manager.allocateDevice(100, options));
@@ -253,6 +254,7 @@ public class DeviceManagerTest extends TestCase {
         DeviceSelectionOptions options = new DeviceSelectionOptions();
         options.setNullDeviceRequested(true);
         EasyMock.expect(mMockAdbBridge.getDevices()).andReturn(new IDevice[] {});
+        EasyMock.expect(mMockDeviceFactory.createDevice()).andReturn(mMockTestDevice);
         replayMocks();
         DeviceManager manager = createDeviceManager();
         assertNotNull(manager.allocateDevice(100, options));
@@ -503,6 +505,7 @@ public class DeviceManagerTest extends TestCase {
         setCheckAvailableDeviceExpectations();
         EasyMock.expect(mMockIDevice.getState()).andReturn(DeviceState.OFFLINE);
         mMockTestDevice.stopLogcat();
+        mMockTestDevice.setDeviceState(TestDeviceState.OFFLINE);
         EasyMock.expect(mMockDeviceFactory.createDevice()).andReturn(mMockTestDevice);
 
         replayMocks();
