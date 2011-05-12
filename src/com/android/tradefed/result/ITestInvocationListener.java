ffcc918cdcb9f1287509a4c3bab2781d37d1bfd8
Brett Chabot <brettchabot@android.com>
2011-05-12 15:05:24 -0700

    ITestInvocationListener javadoc tweaks.
    Change-Id: Id712d1b500be40fcee6ae5e4b1b467c36b2f4f5a

--- src/com/android/tradefed/result/ITestInvocationListener.java
+++ src/com/android/tradefed/result/ITestInvocationListener.java
@@ -22,22 +22,23 @@ import com.android.tradefed.build.IBuildInfo;
  * Listener for test results from the test invocation.
  * <p/>
  * A test invocation can itself include multiple test runs, so the sequence of calls will be
- *  - invocationStarted(BuildInfo)
- *  - testRunStarted
- *  - testStarted
- *  - [testFailed]
- *  - testEnded
- *  ....
- *  - testRunEnded
- *  - [testRunLog+]
- *  ....
- *  - testRunStarted
- *  ...
- *  - testRunEnded
- *  - [testRunLog+]
- *  - [invocationFailed]
- *  - invocationEnded
- *  - getSummary
+ * <ul>
+ * <li>invocationStarted(BuildInfo)</li>
+ * <li>testRunStarted>/li>
+ * <li>testStarted</li>
+ * <li>[testFailed]</li>
+ * <li>testEnded</li>
+ * <li>...</li>
+ * <li>testRunEnded</li>
+ * <li>...</li>
+ * <li>testRunStarted</li>
+ * <li>...</li>
+ * <li>testRunEnded</li>
+ * <li>[invocationFailed]</li>
+ * <li>[testLog+]</li>
+ * <li>invocationEnded</li>
+ * <li>getSummary</li>
+ * </ul>
  * <p/>
  * Note that this is re-using the {@link com.android.ddmlib.testrunner.ITestRunListener}
  * because it's a generic interface. The results being reported are not necessarily device specific.
@@ -46,6 +47,8 @@ public interface ITestInvocationListener extends ITestRunListener {
 
     /**
      * Reports the start of the test invocation.
+     * <p/>
+     * Will be automatically called by the TradeFederation framework.
      *
      * @param buildInfo information about the build being tested
      */
@@ -56,6 +59,9 @@ public interface ITestInvocationListener extends ITestRunListener {
      * <p/>
      * Must be called before {@link #invocationFailed(String, Throwable)} or
      * {@link #invocationEnded()}
+     * <p/>
+     * The TradeFederation framework will automatically call this method, providing the host log
+     * and if applicable, the device logcat.
      *
      * @param dataName a {@link String} descriptive name of the data. e.g. "device_logcat". Note
      *            dataName may not be unique per invocation. ie implementers must be able to handle
@@ -71,6 +77,8 @@ public interface ITestInvocationListener extends ITestRunListener {
     /**
      * Reports that the invocation has terminated, whether successfully or due to some error
      * condition.
+     * <p/>
+     * Will be automatically called by the TradeFederation framework.
      *
      * @param elapsedTime the elapsed time of the invocation in ms
      */
@@ -78,6 +86,8 @@ public interface ITestInvocationListener extends ITestRunListener {
 
     /**
      * Reports an incomplete invocation due to some error condition.
+     * <p/>
+     * Will be automatically called by the TradeFederation framework.
      *
      * @param cause the {@link Throwable} cause of the failure
      */
