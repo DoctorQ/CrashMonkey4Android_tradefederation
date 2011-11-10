201dc5297437fb4e14423e72396259c93ed9ad2f
Omari Stephens <xsdg@android.com>
2011-11-09 16:46:40 -0800

    Require jline
    Bug: 3457774
    Change-Id: Id6677c44120d1ed24bf83c4bdc841c10bd8d350b

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@ LOCAL_JAVACFLAGS := -g
 LOCAL_MODULE := tradefed
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_JAVA_LIBRARIES := junit kxml2-2.3.0 guavalib
+LOCAL_STATIC_JAVA_LIBRARIES := junit kxml2-2.3.0 guavalib jline-1.0
 LOCAL_JAVA_LIBRARIES := ddmlib-prebuilt
 
 include $(BUILD_HOST_JAVA_LIBRARY)
