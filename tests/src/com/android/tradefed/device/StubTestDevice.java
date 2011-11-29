af2692310b1e0a431094c7d9981042b9955a7266
Brett Chabot <brettchabot@android.com>
2011-11-29 12:03:31 -0800

    Manual merge of Remove obsolete API.
    Change-Id: Ia362fccd647417378e05f0478e127b1572c15642

--- tests/src/com/android/tradefed/device/StubTestDevice.java
+++ tests/src/com/android/tradefed/device/StubTestDevice.java
@@ -615,16 +615,6 @@ public class StubTestDevice implements IManagedTestDevice {
      * {@inheritDoc}
      */
     @Override
-    public String installPackage(File packageFile, File certFile, boolean reinstall,
-            String... extraArgs) throws DeviceNotAvailableException {
-        // ignore
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public void setOptions(TestDeviceOptions options) {
         // ignore
     }
