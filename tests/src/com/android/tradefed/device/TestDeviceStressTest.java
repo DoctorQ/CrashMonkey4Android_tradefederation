8aef9ab274deb5a0c3cc2c80dbed4997de6ca693
Yu Shan Emily Lau <yslau@google.com>
2012-01-27 14:11:25 -0800

    Increase the stress loop. Add the test case which push the test folder that contains 200 text file to sdcard for 50 times. The test is to minic the automated test framework userdata pushing.
    Change-Id: I5b0edf5c8082c55d2e17a3e09db78a61242a340a

--- tests/src/com/android/tradefed/device/TestDeviceStressTest.java
+++ tests/src/com/android/tradefed/device/TestDeviceStressTest.java
@@ -15,11 +15,16 @@
  */
 package com.android.tradefed.device;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.android.ddmlib.IDevice;
 import com.android.ddmlib.Log;
 import com.android.ddmlib.testrunner.RemoteAndroidTestRunner;
 import com.android.tradefed.TestAppConstants;
 import com.android.tradefed.result.CollectingTestListener;
 import com.android.tradefed.testtype.DeviceTestCase;
+import com.android.tradefed.util.FileUtil;
 
 /**
  * Long running functional tests for {@link TestDevice} that verify an operation can be run
@@ -30,6 +35,8 @@ import com.android.tradefed.testtype.DeviceTestCase;
 public class TestDeviceStressTest extends DeviceTestCase {
 
     private static final String LOG_TAG = "TestDeviceStressTest";
+    private static final int ITERATIONS = 50;
+    private static final int TEST_FILE_COUNT= 200;
     private TestDevice mTestDevice;
     private IDeviceStateMonitor mMonitor;
 
@@ -40,8 +47,23 @@ public class TestDeviceStressTest extends DeviceTestCase {
         mMonitor = mTestDevice.getDeviceStateMonitor();
     }
 
+    private File createTempTestFiles() throws IOException {
+        File tmpDir = null;
+        File tmpFile = null;
+
+        tmpDir = FileUtil.createTempDir("testDir");
+
+        final String fileContents = "this is the test file contents";
+        for (int i = 0; i < TEST_FILE_COUNT; i++) {
+            tmpFile = FileUtil.createTempFile(String.format("tmp_%d", i), ".txt", tmpDir);
+            FileUtil.writeToFile(fileContents, tmpFile);
+        }
+        return tmpDir;
+
+    }
+
     public void testManyReboots() throws DeviceNotAvailableException {
-        for (int i=0; i < 10; i++) {
+        for (int i=0; i < ITERATIONS; i++) {
             Log.i(LOG_TAG, String.format("testReboot attempt %d", i));
             mTestDevice.reboot();
             assertEquals(TestDeviceState.ONLINE, mMonitor.getDeviceState());
@@ -49,7 +71,7 @@ public class TestDeviceStressTest extends DeviceTestCase {
     }
 
     public void testManyRebootBootloaders() throws DeviceNotAvailableException {
-        for (int i=0; i < 10; i++) {
+        for (int i=0; i < ITERATIONS; i++) {
             Log.i(LOG_TAG, String.format("testRebootBootloader attempt %d", i));
             mTestDevice.rebootIntoBootloader();
             assertEquals(TestDeviceState.FASTBOOT, mMonitor.getDeviceState());
@@ -59,10 +81,43 @@ public class TestDeviceStressTest extends DeviceTestCase {
     }
 
     public void testManyDisableKeyguard() throws DeviceNotAvailableException {
-        for (int i=0; i < 10; i++) {
+        for (int i=0; i < ITERATIONS; i++) {
             Log.i(LOG_TAG, String.format("testDisableKeyguard attempt %d", i));
             mTestDevice.reboot();
             assertTrue(runUITests());
+        }
+    }
+
+    /**
+     * Stress test to push a folder which contains 200 text file to device
+     * internal storage.
+     */
+    public void testPushFolderWithManyFiles() throws IOException, DeviceNotAvailableException {
+        File tmpDir = null;
+        String deviceFilePath = null;
+        String externalStorePath = mTestDevice.getMountPoint(IDevice.MNT_EXTERNAL_STORAGE);
+        assertNotNull(externalStorePath);
+        deviceFilePath = String.format("%s/%s", externalStorePath, "testDir");
+
+        // start the stress test
+        try {
+            // Create the test folder and make sure the test folder doesn't exist in
+            // device before the test start.
+            tmpDir = createTempTestFiles();
+            for (int i = 0; i < ITERATIONS; i++) {
+                mTestDevice.executeShellCommand(String.format("rm -r %s", deviceFilePath));
+                assertFalse(String.format("%s exists", deviceFilePath),
+                        mTestDevice.doesFileExist(deviceFilePath));
+                assertTrue(mTestDevice.pushDir(tmpDir, deviceFilePath));
+                assertTrue(mTestDevice.doesFileExist(deviceFilePath));
+            }
+        } finally {
+            if (tmpDir != null) {
+                FileUtil.recursiveDelete(tmpDir);
+            }
+            mTestDevice.executeShellCommand(String.format("rm -r %s", deviceFilePath));
+            assertFalse(String.format("%s exists", deviceFilePath),
+                    mTestDevice.doesFileExist(deviceFilePath));
         }
     }
 
