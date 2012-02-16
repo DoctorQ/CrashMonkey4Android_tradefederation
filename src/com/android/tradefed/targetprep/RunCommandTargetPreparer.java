a870b637ae6acb2b3f2433824a830d0f8f578c47
Omari Stephens <xsdg@android.com>
2012-02-15 22:53:04 -0800

    Add a target preparer to run an arbitrary command
    Bug: 5880809
    Change-Id: I3cb97c472f35538616307e09dcd8217b053e6af0

---
+++ src/com/android/tradefed/targetprep/RunCommandTargetPreparer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tradefed.targetprep;
+
+import com.android.tradefed.build.IBuildInfo;
+import com.android.tradefed.config.Option;
+import com.android.tradefed.device.DeviceNotAvailableException;
+import com.android.tradefed.device.ITestDevice;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RunCommandTargetPreparer implements ITargetPreparer {
+    @Option(name = "run-command", description = "Command to run")
+    private Collection<String> mCommands = new ArrayList<String>();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setUp(ITestDevice device, IBuildInfo buildInfo) throws TargetSetupError,
+            DeviceNotAvailableException {
+        for (String cmd : mCommands) {
+            // If the command had any output, the executeShellCommand method will log it at the
+            // VERBOSE level; so no need to do any logging from here.
+            final String output = device.executeShellCommand(cmd);
+        }
+    }
+}
+
