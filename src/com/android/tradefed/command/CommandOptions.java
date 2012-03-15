bd7602d13291a528369c690c32618f326015b9ba
Brett Chabot <brettchabot@android.com>
2012-03-15 10:58:59 -0700

    Increase default loop polling time to 10 minutes.
    Change-Id: I4dc1429d9bfa30f3f07043bf94d5139c4f156af7

--- src/com/android/tradefed/command/CommandOptions.java
+++ src/com/android/tradefed/command/CommandOptions.java
@@ -39,7 +39,7 @@ public class CommandOptions implements ICommandOptions {
 
     @Option(name = "min-loop-time", description =
             "the minimum invocation time in ms when in loop mode.")
-    private long mMinLoopTime = 60 * 1000;
+    private long mMinLoopTime = 10 * 60 * 1000;
 
     @Option(name = "loop", description = "keep running continuously.",
             importance = Importance.ALWAYS)
