0ecd0f2466470aba2724ef05a3d9401f6561b48a
Brett Chabot <brettchabot@google.com>
2012-12-13 15:31:31 -0800

    Switch to use ro.build.display.id instead of ro.build.id.
    These two properties are equivalent on Nexus devices, but the 'display' variant
    often has more meaningful data on other devices.
    
    Change-Id: I8411fa02b62f920f3d79426a57a9fd8fcce886b1

--- tests/src/com/android/tradefed/targetprep/DeviceSetupTest.java
+++ tests/src/com/android/tradefed/targetprep/DeviceSetupTest.java
@@ -90,7 +90,8 @@ public class DeviceSetupTest extends TestCase {
         // expect a bunch of shell commands - no need to verify which ones
         EasyMock.expect(mMockDevice.executeShellCommand((String)EasyMock.anyObject())).
                 andReturn("").anyTimes();
-        EasyMock.expect(mMockDevice.getProperty("ro.build.id")).andReturn("IMM76K");
+        EasyMock.expect(mMockDevice.getProperty(ITestDevice.DISPLAYED_BUILD_ID_PROP)).andReturn(
+                "IMM76K");
     }
 
     /**
