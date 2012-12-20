6fb582fac86866e600eb93c97eaaa08e6a4d0517
David Hu <hud@google.com>
2012-12-19 18:49:19 -0800

    Rename DismissKeyguardUtil to DeviceSetupUtil
    Added the ability to key the screen on by holding the wake lock.
    
    Change-Id: I73352960d815efbce5daa1e561c74a50b9702d6e

--- util-apps/DeviceSetupUtil/Android.mk
+++ util-apps/DeviceSetupUtil/Android.mk
@@ -20,7 +20,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SDK_VERSION := 8
-LOCAL_PACKAGE_NAME := DismissKeyguardUtil
+LOCAL_PACKAGE_NAME := DeviceSetupUtil
 
 include $(BUILD_PACKAGE)
 
